06ad623b8f766d51dc70aa8cce9bb7e2153f598d
Gerson Diaz <gersondiaz030998@gmail.com>
2024-11-17 23:52:30 -0500

    Update version to 0.6 and modify chat input handling in PlanificadorAgent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='danilo-planner',
-    version='0.5',
+    version='0.6',
     packages=['danilo_planner'],
     description='Planificador de proyectos con IA mediante GPT-4',
     author='Danilo Florez',
