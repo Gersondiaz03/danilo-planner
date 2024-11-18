2fcac7798e0f7d5df14279023eb57e4c57cc64b7
Gerson Diaz <gersondiaz030998@gmail.com>
2024-11-17 23:12:42 -0500

    Fix path separator in model loading and update version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='danilo-planner',
-    version='0.2',
+    version='0.3',
     packages=['danilo_planner'],
     description='Planificador de proyectos con IA mediante GPT-4',
     author='Danilo Florez',
